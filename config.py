eea81fb1cfacc6ee08fefc46102cdb1b82686f39
NineOceans <44770303+LyuLumos@users.noreply.github.com>
2023-09-22 09:08:08 +0000

    update config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "sk-xxx"
+openai.api_base = "https://openai.lyujiuyang05275.workers.dev/v1"
 
 CHAT_MODEL = "gpt-3.5-turbo"
 EMBEDDING_MODEL = "text-embedding-ada-002"
